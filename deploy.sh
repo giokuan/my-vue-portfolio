e04dd04dff9c3ea54890498fdef8774477d7ffe8
giovanni kuan <giokuan@gmail.com>
2024-01-19 08:08:34 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<giokuan>/<giokuan>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<giokuan>/my-vue-portfolio.git main:gh-pages
+git push -f git@github.com:giokuan/my-vue-portfolio.git main:gh-pages
 
 cd -
